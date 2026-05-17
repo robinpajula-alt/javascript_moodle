62f630d0ff3af3dc658e3a08120ae2551d34427c
Robin <robin.pajula@gmail.com>
2026-05-17 22:31:51 +0300

    järjend_muutmine exercise finished

--- Javascript moodle - Järjendi muutmine/järjend_muutmine.js
+++ Javascript moodle - Järjendi muutmine/järjend_muutmine.js
@@ -1,5 +1,7 @@
 let words = ['allveelaev', 'sügavas', 'käis', 'linnud', 'vees', 'kollane']
 document.getElementById('järjend').innerHTML = `<h3>Järjend on: ${words}</h3>`
-const sona_eemaldaja = () => {
 
+const sona_eemaldaja = () => {
+    let new_words = words.filter(word => word !== document.getElementById('sona').value);
+    document.getElementById('tulemus').innerHTML = `<h3>Tulemus on: ${new_words}</h3>`
 }
