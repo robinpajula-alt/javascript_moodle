e01ee6017f97a1370b9723e4f877671e1acbd481
Robin <robin.pajula@gmail.com>
2026-05-17 14:10:15 +0300

    Changed folder names again and started järjend_muutumine exercise

---
+++ Javascript moodle - Järjendi muutmine/järjend_muutmine.js
@@ -0,0 +1,5 @@
+let words = ['allveelaev', 'sügavas', 'käis', 'linnud', 'vees', 'kollane']
+document.getElementById('järjend').innerHTML = `<h3>Järjend on: ${words}</h3>`
+const sona_eemaldaja = () => {
+
+}
