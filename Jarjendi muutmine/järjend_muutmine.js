e5ed37dc1d9b07dacbc164462a78427e42ae8b4c
Robin <robin.pajula@gmail.com>
2026-05-17 22:35:30 +0300

    change Järjendi muutmine to Jarjendi muutmine because cPanel doesnt understand UTF-8 letters

--- Jarjendi muutmine/järjend_muutmine.js
+++ Jarjendi muutmine/järjend_muutmine.js
@@ -4,4 +4,4 @@ document.getElementById('järjend').innerHTML = `<h3>Järjend on: ${words}</h3>`
 const sona_eemaldaja = () => {
     let new_words = words.filter(word => word !== document.getElementById('sona').value);
     document.getElementById('tulemus').innerHTML = `<h3>Tulemus on: ${new_words}</h3>`
-}
+} 
