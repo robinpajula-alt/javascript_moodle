dea6a9a45d04de2626d94456a65b9841b53a5220
Robin <robin.pajula@voco.ee>
2026-05-22 15:00:25 +0300

    tegin setInterval iteratori ülesande ära

---
+++ Nord Pool igal pool/nord_pool.js
@@ -0,0 +1,10 @@
+const kast1 = document.getElementById("kast1");
+const kast2 = document.getElementById("kast2");
+const kast3 = document.getElementById("kast3");
+const kast4 = document.getElementById("kast4");
+setInterval(function() {
+    kast1.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    kast2.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    kast3.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    kast4.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+}, 1000)
